58c686e87c24b3e5ca828dd519293b9f982f77c7
gregorydemay <gregory.demay@dfinity.org>
2026-04-09 16:17:26 +0200

    DEFI-2758: polishing

--- scripts/ci_post_run_benchmark.sh
+++ scripts/ci_post_run_benchmark.sh
@@ -2,5 +2,7 @@
 set -Eexuo pipefail
 
 if [ "$EXIT_STATUS" -eq 1 ]; then
+  echo "canbench_results.yml is not up to date."
+  echo "If the performance change is expected, run 'canbench --persist' locally and commit the updated results."
   exit 1
 fi
